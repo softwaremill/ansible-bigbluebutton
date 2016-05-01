6e9478352dfe69dcdd416e4c8985b15aa47883c7
Jan Rotter <jan.rotter@gmail.com>
2016-05-01 20:54:34 +0200

    Add set -e to test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
+set -e
+
 export ANSIBLE_ROLES_PATH='../'
 INVENTORY='localhost,'
 CONNECTION='local'
 
 ansible-playbook -i $INVENTORY tests/test.yml --syntax-check
 ansible-playbook -i $INVENTORY tests/test.yml --connection=$CONNECTION --sudo --verbose --skip-tags dist_upgrade
-ansible-playbook -i $INVENTORY tests/test.yml --connection=$CONNECTION --sudo --skip-tags non_idempotent \
-  | grep --quiet 'changed=0.*failed=0'
-if [ $? -eq 0 ]; then
+if ansible-playbook -i $INVENTORY tests/test.yml --connection=$CONNECTION --sudo --skip-tags non_idempotent \
+  | grep --quiet 'changed=0.*failed=0'; then
   echo 'Idempotency: PASS' && exit 0
 else
   echo 'Idempotency: FAIL' && exit 1
