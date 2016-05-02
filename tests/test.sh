21d54c97b54fbee6b11311b70b3c4ac6a9696653
Jan Rotter <jan.rotter@gmail.com>
2016-05-02 07:45:16 +0200

    Extract variables to defaults and split bbb installation to allow skipping bbb-demo and bbb-check.

--- tests/test.sh
+++ tests/test.sh
@@ -7,11 +7,10 @@ INVENTORY='localhost,'
 CONNECTION='local'
 
 ansible-playbook -i $INVENTORY tests/test.yml --syntax-check
-ansible-playbook -i $INVENTORY tests/test.yml --connection=$CONNECTION --sudo --skip-tags dist_upgrade
-if ansible-playbook -i $INVENTORY tests/test.yml --connection=$CONNECTION --sudo --skip-tags non_idempotent \
+ansible-playbook -i $INVENTORY tests/test.yml --connection=$CONNECTION --sudo
+if ansible-playbook -i $INVENTORY tests/test.yml --connection=$CONNECTION --sudo \
   | grep --quiet 'changed=0.*failed=0'; then
   echo 'Idempotency: PASS' && exit 0
 else
   echo 'Idempotency: FAIL' && exit 1
 fi
-
